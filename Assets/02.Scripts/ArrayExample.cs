0b5b1de31f39c9fb61f2afc424c28c9b0989d296
whqlskqTy2 <kimhhss06@gmail.com>
2025-01-20 15:01:06 +0900

    범위 및 액세스 한정자

---
+++ Assets/02.Scripts/ArrayExample.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+//[System.Diagnostics.DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+//public class ArrayExample : MonoBehaviour
+/*{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*string[] Avengers = new string[]{ "Iron man", "Captain America","Spider man"};
+        string[] Justice = new string[3];
+        Justice[0] = "Super Man";
+        Justice[1] = "Bat Man";
+        Justice[2] = "Aqua Man";
+        Justice[3] = "Wonder Woman";
+
+        string[,]Heroes = new string[2,3];
+        Heroes[0,0] = "Iron Man";
+        //
+        Heroes[1,2]= "Aqua Man";*/
+        //var Heroes = new string[,]{{"IM","CA","SM"},{"SM","BM","AM"}};
+        //string[][]NewHeroes = new string[2][];
+        //NewHeroes[0]= new string [3]{"IM","CA","SM"};
+        //NewHeroes[1]= new string [4]{"SM","BM","AM","WW"};
+        //string[][] NewHeroes = new string[][]
+        //{
+            //new string[]{"IM","CA","SM"},
+           // new string[]{"SM","BM","AM","WW"}
+        //};
+        
+       // Debug.Log(Heroes[1,1]);
+        //Debug.Log(NewHeroes[1][1]);
+
+        //public void TwoDA2OneDA()
+        //{
+          //  int[,] array2d=new int[,]{{1,2},{3,4},{5,6}};
+            //int[]array1d
+            //=new int[array2d.GetLength(0)*array2d.GetLength(1)];
+
+            //for (int i = 0; i < array2d.GetLength(0); i++)
+            //for (int k = 0; k < array2d.GetLength(1); k++)
+            //{
+              //  array1d[array2d.GetLength(1) * i + k]
+               // = array2d[i,k];
+            //}
+        //}
+    //}
+//}
+
+
