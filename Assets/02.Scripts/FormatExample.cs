aa51682d38e8ebde8b4329e8219528f12341a54a
hgkang2 <hgkang2hank@naver.com>
2025-01-21 15:48:23 +0900

    Update FormatExample.cs

--- Assets/02.Scripts/FormatExample.cs
+++ Assets/02.Scripts/FormatExample.cs
@@ -7,7 +7,9 @@ public class FormatExample : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.LogFormat("I am {0}. I am {1} years old.", "Iron Man", 55);   
+        Debug.LogFormat("I am {0}. I am {1} years old.", "Super Man", 100); 
+        Debug.LogFormat("I am {0}. You are not {0}. ", "Thor"); 
     }
 
     // Update is called once per frame
