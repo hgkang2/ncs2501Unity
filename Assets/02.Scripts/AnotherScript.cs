64fff252b86897b586184c26b00cd3b203475f75
ilsang <hgkang2hank@naver.com>
2025-01-24 15:48:14 +0900

    복습좀

---
+++ Assets/02.Scripts/AnotherScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherScript : MonoBehaviour
+{
+    public string playerScore { get; internal set; }
+
+    // Start is called before the first frame update
+    {
+   public int playerScore = 9001;
+    }
+}
---
+++ Assets/02.Scripts/yetAnotherScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yetAnotherScript : MonoBehaviour
+{
+    public string numberOfPlayerDeaths { get; internal set; }
+
+  {
+    public int numberOfPlayerDeaths = 3;
+}
+}
